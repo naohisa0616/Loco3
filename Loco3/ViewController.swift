b57d27839feb9d205ee82ed53067494e780e49f4
naohisa0616 <naohisa0616@icloud.com>
2019-11-18 13:31:02 +0900

    Initial Commit

---
+++ Loco3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loco3
+//
+//  Created by 宮崎直久 on 2019/11/18.
+//  Copyright © 2019 宮崎直久. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
